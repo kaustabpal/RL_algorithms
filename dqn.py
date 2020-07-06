54b75dcfcf79c35ff80109904ce9eca8202c0e97
Kaustab Pal <kaustab21@gmail.com>
2020-07-07 05:21:53 +0530

    hardupdate 100 script

--- dqn.py
+++ dqn.py
@@ -131,9 +131,9 @@ if __name__=="__main__":
     EPISODES = 1000 # total nusmber of episodes to train for
     soft_update = False
     # for future experiments, only change these three values
-    UPDATE_TARGET_INTERVAL = 50  # Used when hard update is used 
+    UPDATE_TARGET_INTERVAL = 100  # Used when hard update is used 
     TAU = 0.0001 # used when soft update is used
-    target_dir = "hard_update_50" # hard_update_20 50 100 200
+    target_dir = "hard_update_100" # hard_update_20 50 100 200
 
     temp_env = gym.make("CartPole-v1")
 
