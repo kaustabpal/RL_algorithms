2f3e1f049d3a603d17cc47557af6f225a9cfea8c
Kaustab Pal <kaustab21@gmail.com>
2020-07-11 02:49:41 +0530

    modified code

--- dqn.py
+++ dqn.py
@@ -21,25 +21,38 @@ logging.disable(logging.WARNING)
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 ################################################################
 
-class ReplayBuffer:
-    def __init__(self):
-        self.buffer = collections.deque(maxlen=BUFFERLIMIT)
+class ReplayBuffer: # Replay buffer class
+    def __init__(self, bufferlimit):
+        self.bufferlimit = bufferlimit
+        self.buffer = collections.deque(maxlen= self.bufferlimit)
 
     def put(self, transition):
-        if(self.size() >= BUFFERLIMIT):
+        '''
+        Function used to insert experiences in the experience replay buffer
+        '''
+        if(self.size() >= self.bufferlimit):
             self.buffer.pop()
         self.buffer.append(transition)
 
     def sample(self, mini_batch_size=32):
+        '''
+        Function used to randomly sample MINI_BATCH_SIZE of experiences from the experience replay buffer.
+        '''
         mini_batch = random.sample(self.buffer, min(len(self.buffer), mini_batch_size))
         return mini_batch
     
     def size(self):
+        '''
+        Returns the total size of the experience replay buffer.
+        '''
         return len(self.buffer)
 ######################################################################
 
-class Qnet:
+class Qnet: # Q-network class
     def __init__(self, hidden_layers = 2, observation_space = 4, action_space = 2, learning_rate = 0.0001, units = 64):
+        '''
+        creates the model of the neural network with the given hyperparameters.
+        '''
         self.input_shape = observation_space
         self.output_shape = action_space
         self.learning_rate = learning_rate
@@ -47,20 +60,24 @@ class Qnet:
         
         self.model = tf.keras.models.Sequential()
         self.model.add(tf.keras.layers.Dense(self.units, input_shape=(self.input_shape,), activation = "relu")) # 1st hidden layer
-        for i in range(hidden_layers-1):
+
+        for i in range(hidden_layers-1): # create the rest of the hidden layers
             self.model.add(tf.keras.layers.Dense(self.units, activation = "relu"))
 
-        self.model.add(tf.keras.layers.Dense(self.output_shape, activation = "linear"))
+        self.model.add(tf.keras.layers.Dense(self.output_shape, activation = "linear")) # create output layer
 
         self.model.compile(optimizer =tf.keras.optimizers.Adam(lr=self.learning_rate), loss="mse")
     
     def train(self,q_target,replay,mini_batch_size=32):
-        mini_batch = replay.sample(mini_batch_size)
+        '''
+        Function to train the main Q-network.
+        '''
+        mini_batch = replay.sample(mini_batch_size) 
         x = []
         y = []
         for s,a,r,s_prime,done in mini_batch:
             max_future_q = np.amax(q_target.model.predict(tf.constant(s_prime,shape=(1,self.input_shape))))
-            target = r + DISCOUNT_RATE*max_future_q*np.invert(done)
+            target = r + DISCOUNT_RATE*max_future_q*np.invert(done) # calculate target using Q-target
             current_q = self.model.predict(tf.constant(s,shape=(1,self.input_shape))) # current q_values for the actions
             current_q[0][a] = target # updating the q_value of the chosen action to that of the target q value
             x.append(s)
@@ -70,14 +87,17 @@ class Qnet:
         self.model.fit(x,y)
 
     def update_weight(self, q, ep_num, update_interval = 20, tau = 0.01, soft=False):
+        '''
+        Function to update the weights of the target network Q-target.
+        '''
         if soft == False:
-            # hard update
+            # perform hard update on the target network
             if(ep_num % update_interval == 0):
                 target_theta = q.model.get_weights()
                 self.model.set_weights(target_theta)
                 print("Target Update")
         else:
-            # soft update
+            # perform soft update on the target network
             q_theta = q.model.get_weights()
             target_theta = self.model.get_weights()
             counter = 0
@@ -88,6 +108,9 @@ class Qnet:
             self.model.set_weights(target_theta)
     
     def sample_action(self, obs, epsilon):
+        '''
+        Function to select an action depending on the observation using epsilon greedy approach.
+        '''
         coin = random.random()
         if(coin<=epsilon):
             return random.randint(0, 1) # returns values between 0 and 1
@@ -95,27 +118,33 @@ class Qnet:
             return np.argmax(self.model.predict(obs))
 ######################################################################
 
-def test(n_epi, epsilon, q, seed_val, vid_dir, frame_number):
-    rwrd = []
-    test_env = gym.make("CartPole-v1")
+def test(n_epi, epsilon, q, seed_val, vid_dir): 
+
+'''
+Function to test the performance of the DQN agent.
+The agent is run in a new test environment for 10 episodes.
+The mean score is used to determine the performance of the agent.
+'''
+    rwrd = [] # vector to store the rewards
+    test_env = gym.make("CartPole-v1") # new test environment
     test_env.seed(seed_val) # setting seed for the test_env
     for eps in range(10):        
-        test_s = test_env.reset()
+        test_s = test_env.reset() # present state
         test_done = False
         test_score = 0
         while not test_done:
-            test_a = np.argmax(q.model.predict(tf.constant(test_s,shape=(1,input_shape))))
-            test_s_prime, test_r, test_done, test_info =test_env.step(test_a)
-            test_s = test_s_prime
-            test_score += test_r # we get +1 reward every timestep
+            test_a = np.argmax(q.model.predict(tf.constant(test_s,shape=(1,input_shape)))) # select action with epsilon-greedy policy
+            test_s_prime, test_r, test_done, test_info =test_env.step(test_a) # get next state, rewards, done flag
+            test_s = test_s_prime 
+            test_score += test_r # update score for the current episode
             if test_done:
                 rwrd.append(test_score)
                 break
-    #frame_number = record_video(test_env, q , n_epi, vid_dir, frame_number)
-    mean_score = np.mean(rwrd)
-    std_score = round(np.std(rwrd),3)
+ 
+    mean_score = np.mean(rwrd) # calculate the mean score of the 10 test episodes
+    std_score = round(np.std(rwrd),3) # calculate the standard deviation in the scores  
     print("Episode: {}. Score: {}. Std: {}. Epsilon: {}".format(n_epi, mean_score, std_score, round(epsilon,3)))
-    return mean_score, std_score, frame_number  
+    return mean_score, std_score 
 
 
 if __name__=="__main__":
@@ -127,39 +156,64 @@ if __name__=="__main__":
     HIDDEN_LAYER_UNITS = 64
     LEARNING_RATE = 0.0005
     DISCOUNT_RATE  = 0.99 
-    EPISODES = 2000 # total nusmber of episodes to train for
+    EPISODES = 2000
     UPDATE_TARGET_INTERVAL = 100  # target update interval for hard update
     TAU = 0.0001 # used when soft update is used
     ############################################
 
     seed_val = 0
-    soft_update = True
+    soft_update = True # Set to True if you want to do soft update instead of hard update
     log_dir = "final_experiment" # directory name to store log data
     exp_name = "final_experiment" # name of the experiment
-    plot_title = "DQN-CartPole. Final Experiment" # title of the plot
+    plot_title = "DQN-CartPole. Final Experiment" # Title of the plot
 
     #############################################    
-
+    # initialize random seeds
     np.random.seed(seed_val)
     tf.random.set_seed(seed_val)
     random.seed(seed_val)
 
+    #############################################
+
     env = gym.make("CartPole-v1") # select environment. Currently only tested on CartPole-v1
     env.seed(seed_val)  # setting seed for the env
+
+    #############################################
+    # create the main Q-network, the target network Q-target and the experience replay buffer
+
     input_shape = env.observation_space.shape[0] # input shape for the neural net
     output_shape = env.action_space.n # output shape for the neural net
 
-    q = Qnet(hidden_layers = HIDDEN_LAYERS, observation_space = input_shape, action_space = output_shape, learning_rate = LEARNING_RATE, units = HIDDEN_LAYER_UNITS) # the main Q-network
-    q_target = Qnet(hidden_layers = HIDDEN_LAYERS, observation_space = input_shape, action_space = output_shape, learning_rate = LEARNING_RATE, units = HIDDEN_LAYER_UNITS) # the target network Q-target
-    q_target.model.set_weights(q.model.get_weights()) # initializing Q-target weights to the weights of the main Q-network
-    memory = ReplayBuffer() # Experience Replay Buffer
+    q = Qnet(
+        hidden_layers = HIDDEN_LAYERS, 
+        observation_space = input_shape, 
+        action_space = output_shape, 
+        learning_rate = LEARNING_RATE, 
+        units = HIDDEN_LAYER_UNITS
+        ) # the main Q-network
 
+    q_target = Qnet(
+        hidden_layers = HIDDEN_LAYERS, 
+        observation_space = input_shape, 
+        action_space = output_shape, 
+        learning_rate = LEARNING_RATE, 
+        units = HIDDEN_LAYER_UNITS
+        ) # the target network Q-target
+
+    q_target.model.set_weights(q.model.get_weights()) # initializing Q-target weights to the weights of the main Q-network
+
+    memory = ReplayBuffer(bufferlimit = BUFFERLIMIT) # Experience Replay Buffer
+
+    #############################################
+    
     ep_vec = [] # Vector to store ep_number for plotting
     mean_score_vec = [] # vector to store score in this episode for plotting
     std_vec =[] # vector to store standard deviation of rewards
-    frame_number = 0 # frame number of progress video
+
+    #############################################
 
     savename = exp_name
+    
     performance_dir = "Performance/"+log_dir+"/"
     if not os.path.exists(performance_dir):
         os.mkdir(performance_dir)
@@ -174,17 +228,17 @@ if __name__=="__main__":
     vid_dir = "Video/"+log_dir+"/"
     if not os.path.exists(vid_dir):
         os.mkdir(vid_dir)
-    save_vid_cmd = "ffmpeg -y -framerate 12 -i "+vid_dir+"image%d.png -c:v libx264 -profile:v high -crf 20 -pix_fmt yuv420p progress.mp4"
 
-    start_time = timeit.default_timer()
+    #############################################
 
-    ##################################################################
     for n_epi in range(EPISODES+1):
         epsilon = max(0.01, (0.99 - 0.98/200*n_epi))
         s = env.reset()
         done = False
         score = 0.
-        ##############################################################
+
+        #############################################
+
         while not done:
             a = q.sample_action(tf.constant(s,shape=(1,input_shape)), epsilon) #select action from updated q net
             s_prime, r, done, info = env.step(a)
@@ -193,7 +247,9 @@ if __name__=="__main__":
             score += r
             if done:
                 break
-        ###############################################################        
+
+        #############################################
+
         if(memory.size() >= 1000):
             q.train(q_target, memory, MINI_BATCH_SIZE)    # update q net
             q_target.update_weight(q, ep_num=n_epi, update_interval=UPDATE_TARGET_INTERVAL, tau=TAU, soft=soft_update)
@@ -205,29 +261,10 @@ if __name__=="__main__":
             ep_model =  save_model+"_"+str(n_epi)
             if not os.path.exists(ep_model):
                 os.mkdir(ep_model)
-            q.model.save_weights(ep_model+"/"+"DQN_"+savename+"_"+str(n_epi))      
-    ####################################################################   
+            q.model.save_weights(ep_model+"/"+"DQN_"+savename+"_"+str(n_epi))   
 
-    # ##### plot showing score vs episodes #######
-    # y_max=list(map(add, mean_score_vec, std_vec))
-    # y_min=list(map(sub, mean_score_vec, std_vec))
-    # plt.ylim((0,500))
-    # plt.xlim((0,EPISODES-10))
-    # plt.xlabel('Episodes')
-    # plt.ylabel('Rewards')
-    # plt.title(plot_title)
-    # plt.plot(ep_vec,mean_score_vec)
-    # plt.fill_between(ep_vec, y_min, y_max, alpha=0.1)
-    
-    ########################################
+    #############################################   
 
-    ############ SAVING DATAS ###########################
     performance_data = {'Episode':ep_vec, 'mean_score': mean_score_vec, 'std_dev': std_vec}
     df = pd.DataFrame(performance_data) 
     df.to_csv(save_performance, index=False)
-
-    #q.model.save_weights(save_model)
-
-    #os.system(save_vid_cmd)
-    stop_time = timeit.default_timer()
-    print("TIME TAKEN: {}".format(stop_time-start_time))
--- render.py
+++ render.py
@@ -1,12 +1,11 @@
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 import gym
-from gym import wrappers
 import random
-import collections
 import numpy as np
 import tensorflow as tf
 from dqn import Qnet
-import time
+
 ################################################################
 # suppress tensorflow logs. 
 # Solution from https://stackoverflow.com/a/57497465
@@ -14,52 +13,31 @@ import logging, os
 logging.disable(logging.WARNING) 
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 ################################################################
-
-# def record_video(q , n_epi, vid_dir, frame_number):
-#     rec_env = wrappers.Monitor(rec_env, "/tmp/CartPole-v1", video_callable = False, force = True)
-#     s = rec_env.reset()
-#     done = False
-#     plt.imshow(rec_env.render(mode='rgb_array'))
-#     plt.title("DQN-CartPole. | Episode: %d" % (n_epi))
-#     plt.axis('off')        
-#     plt.savefig(vid_dir+"image"+str(frame_number)+".png")
-    
-#     while not done:
-#         a = np.argmax(q.model.predict(tf.constant(s,shape=(1,input_shape))))
-#         s_prime, _ , done, _ = rec_env.step(a)
-#         s = s_prime
-#         frame_number += 1
-#         plt.imshow(rec_env.render(mode='rgb_array'))
-#         plt.title("DQN-CartPole. | Episode: %d" % (n_epi))
-#         plt.axis('off')  
-#         plt.savefig(vid_dir+"image"+str(frame_number)+".png")
-#         if done:
-#             rec_env.close()
-#             break
-#     return frame_number
-
+Writer = animation.writers['ffmpeg']
+writer = Writer(fps=120, metadata=dict(artist='Me'), bitrate=1800)
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ims = []
 ######## hyperparameters###################
-BUFFERLIMIT = 50_000
-MINI_BATCH_SIZE = 32 
+# Set up formatting for the movie files
 HIDDEN_LAYERS = 2
 HIDDEN_LAYER_UNITS = 64
 LEARNING_RATE = 0.0005
 DISCOUNT_RATE  = 0.99 
 EPISODES = 40 # total nusmber of episodes to train for
-UPDATE_TARGET_INTERVAL = 100  # target update interval for hard update
-TAU = 0.0001 # used when soft update is used
 ############################################
-
 seed_val = 0
 env = gym.make("CartPole-v1") # select environment. Currently only tested on CartPole-v1
+###############################
 env.seed(seed_val)
 np.random.seed(seed_val)
 tf.random.set_seed(seed_val)
 random.seed(seed_val)
-
+################################
 log_dir = "final_experiment" # directory name to store log data
 exp_name = "final_experiment" # name of the experiment
 savename = exp_name
+
 model_dir = "Model/"+log_dir+"/"
 if not os.path.exists(model_dir):
     os.mkdir(model_dir)    
@@ -68,12 +46,10 @@ model_dir = model_dir+"DQN_"+savename
 vid_dir = "Video/"+log_dir+"/"
 if not os.path.exists(vid_dir):
     os.mkdir(vid_dir)
-save_vid_cmd = "ffmpeg -y -framerate 12 -i "+vid_dir+"image%d.png -c:v libx264 -profile:v high -crf 20 -pix_fmt yuv420p progress.mp4"
 
+################################
 input_shape = env.observation_space.shape[0] # input shape for the neural net
 output_shape = env.action_space.n # output shape for the neural net
-frame_number = 0
-
 q = Qnet(
     hidden_layers = HIDDEN_LAYERS, 
     observation_space = input_shape, 
@@ -81,32 +57,34 @@ q = Qnet(
     learning_rate = LEARNING_RATE, 
     units = HIDDEN_LAYER_UNITS
     ) # the main Q-network
-
-for i in range(0,41,10):
-    print(i)
+################################
+for i in range(0,EPISODES+1,10):
     ep_model =  model_dir+"_"+str(i)
     q.model.load_weights(ep_model+"/"+"DQN_"+savename+"_"+str(i))
     s = env.reset()
     score = 0.
     done = False
-    plt.imshow(env.render(mode='rgb_array'))
-    plt.title("DQN-CartPole. | Episode: %d" % (i))
-    plt.axis('off')        
-    plt.savefig(vid_dir+"image"+str(frame_number)+".png")
+    im = plt.imshow(env.render(mode='rgb_array'))
+    ttl = plt.text(0.5,1.01,"DQN-CartPole-v1. | Episode: "+str(i), horizontalalignment = 'center', verticalalignment = 'bottom', transform = ax.transAxes)
+    plt.axis('off')
+    ims.append([im, ttl])  
+
     ##############################################################
     while not done:
         a = np.argmax(q.model.predict(tf.constant(s,shape=(1,input_shape))))
         s_prime, r , done, _ = env.step(a)
         s = s_prime
         score += r
-        frame_number += 1
-        plt.imshow(env.render(mode='rgb_array'))
-        plt.title("DQN-CartPole. | Episode: %d" % (i))
+        im=plt.imshow(env.render(mode='rgb_array'))
+        ttl = plt.text(0.5,1.01,"DQN-CartPole-v1. | Episode: "+str(i), horizontalalignment = 'center', verticalalignment = 'bottom', transform = ax.transAxes)
         plt.axis('off')  
-        plt.savefig(vid_dir+"image"+str(frame_number)+".png")
-        time.sleep(1/120)
+        ims.append([im, ttl])
+        #plt.savefig(vid_dir+"image"+str(frame_number)+".png")
+        #time.sleep(1/120)
         if done:
-            env.close()
+            # env.close()
             break
     print("Episode: {}. Rewards: {}".format(i,score))
-os.system(save_vid_cmd)
+
+ani = animation.ArtistAnimation(fig, ims, interval=80, blit=True)
+ani.save(vid_dir+'progress.mp4')
